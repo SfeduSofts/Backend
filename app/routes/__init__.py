9a58fd92a1087eaeb77d4042f15b3912881328ed
HopenPen <hopen_pen123@mail.ru>
2026-04-29 22:34:12 +0300

    Исправил ошибки, свзяанные с запуском сервера

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,6 @@
 from app.routes.projects import router as project_router
 from app.routes.students import router as student_router
 from app.routes.teams import router as team_router
+from app.routes.auth import router as auth_router
 
-__all__ = ["project_router", "student_router", "team_router"]
+__all__ = ["project_router", "student_router", "team_router", "auth_router"]
